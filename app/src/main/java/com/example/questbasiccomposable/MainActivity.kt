0bbe58d3de0d7545b17f6c223ddbb76095853e65
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-10-19 00:40:56 +0700

    Inisialisasi komponen MainActivity dan struktur dasar aplikasi

--- app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
@@ -16,32 +16,6 @@ import com.example.questbasiccomposable.ui.theme.QuestBasicComposableTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            QuestBasicComposableTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuestBasicComposableTheme {
-        Greeting("Android")
-    }
-}
