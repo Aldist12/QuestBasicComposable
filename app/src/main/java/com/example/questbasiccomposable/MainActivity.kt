be0a7e34dd95131c60eb2235eabcf7c55c927101
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-10-19 00:50:23 +0700

    Mengatur tata letak kolom

--- app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.questbasiccomposable.ui.theme.QuestBasicComposableTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,5 +31,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LoginScreen() {
-
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(50.dp)
+    ) {
+}
 }
