87dd282cadcd05a8a0460b4028fbfb1d427a9063
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-10-19 01:20:24 +0700

    Tampilkan file photo myfoto sebagai gambar profil pengguna

--- app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
@@ -12,11 +12,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -95,8 +97,17 @@ fun LoginScreen() {
             )
         )
         Spacer(modifier = Modifier.height(30.dp))
+        Image(
+            painter = painterResource(id = R.drawable.myfoto),
+            contentDescription = "Profil",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(400.dp)
+                .clip(RoundedCornerShape(16.dp))
+        )
     }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun LoginScreenPreview() {
