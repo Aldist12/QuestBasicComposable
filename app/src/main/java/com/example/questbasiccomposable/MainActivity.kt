25fe77946109ac4bd7226f1fb973ee659e1f096f
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-10-19 01:05:48 +0700

    Tampilkan file photo UMY sebagai Logo

--- app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
@@ -4,21 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.composablelayout.R
 import com.example.questbasiccomposable.ui.theme.QuestBasicComposableTheme
 
 class MainActivity : ComponentActivity() {
@@ -56,6 +61,13 @@ fun LoginScreen() {
                 fontSize = 16.sp,
                 color = Color.Gray
             )
+        )
+        Image(
+            painter = painterResource(id = R.drawable.umy),
+            contentDescription = "Logo UMY",
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .size(250.dp)
         )
 }
 }
